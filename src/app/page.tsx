8440cb88a5b689d653fedac33fe08a6e3378b701
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-12 03:01:11 +0000

    mira haz que la pagina saea mas grande un poco mas para que el cuadro de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { PromotionsCarousel } from '@/components/promotions-carousel';
 import { SportsSidebar } from '@/components/sports-sidebar';
 import { Sportsbook } from '@/components/sportsbook';
 import { BetSlip } from '@/components/bet-slip';
-import { Footer } from '@/components/layout/footer';
 import { BetSlipProvider } from '@/contexts/bet-slip-context';
 
 export default function Home() {
@@ -15,11 +14,11 @@ export default function Home() {
         <MainNav />
         <main className="container mx-auto flex-grow px-2 py-4 md:px-4">
           <PromotionsCarousel />
-          <div className="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-4">
+          <div className="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-5">
             <aside className="hidden lg:col-span-1 lg:block">
               <SportsSidebar />
             </aside>
-            <div className="col-span-1 lg:col-span-2">
+            <div className="col-span-1 lg:col-span-3">
               <Sportsbook />
             </div>
             <aside className="hidden lg:col-span-1 lg:block">
