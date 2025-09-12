9ae2e2e1f4cd08be9fdac65d462cc53855be9144
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-12 02:46:13 +0000

    Fútbol - Premier League Error Error de API: Unknown sport

--- src/lib/sports-data.ts
+++ src/lib/sports-data.ts
@@ -1,7 +1,7 @@
 export const apiSports = [
     { key: 'soccer_south_america_copa_sudamericana', title: 'Fútbol - Copa Sudamericana' },
     { key: 'soccer_spain_la_liga', title: 'Fútbol - La Liga España' },
-    { key: 'soccer_england_premier_league', title: 'Fútbol - Premier League' },
+    { key: 'soccer_epl', title: 'Fútbol - Premier League' },
     { key: 'soccer_uefa_champions_league', title: 'Fútbol - Champions League' },
     { key: 'basketball_nba', title: 'Baloncesto - NBA' },
     { key: 'tennis_wimbledon', title: 'Tenis - Wimbledon' },
