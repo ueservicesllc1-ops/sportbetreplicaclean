72bdc809311caa4c2bf660a2731df76ca139a4b9
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-13 23:24:01 +0000

    Error: An unexpected response was received from the server.     at fetch

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,9 +4,13 @@ import * as admin from 'firebase-admin';
 // Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
   try {
+    const serviceAccount = JSON.parse(
+        process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+    );
     // When initialized without arguments, the SDK will look for the 
     // `GOOGLE_APPLICATION_CREDENTIALS` environment variable.
     admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
   } catch (error: any) {
