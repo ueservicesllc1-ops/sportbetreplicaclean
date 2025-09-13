1ca7af3cdaea4701f063a0ec96f32cc530aa0f3e
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-13 22:49:17 +0000

    var admin = require("firebase-admin");
    var serviceAccount = require("pa

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,12 +3,14 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
+    const serviceAccount = {
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
+    };
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
   } catch (error: any) {
