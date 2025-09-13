41bee580a299020ec977c00aec0ee54dabcbf821
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-13 03:10:27 +0000

    https://freeimage.host/i/KTE9RXj

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
     <Image
-      src="/logo.jpg"
+      src="https://iili.io/KTE9RXj.png"
       alt="SportBet Replica Logo"
       width={150}
       height={38}
