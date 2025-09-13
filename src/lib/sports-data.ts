c455cb9f053312e006a2b2eb5a89d2e71203f136
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-13 03:11:00 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/sports-data.ts
+++ src/lib/sports-data.ts
@@ -1,10 +1,11 @@
+
 export const apiSports = [
-    { key: 'soccer_south_america_copa_sudamericana', title: 'Fútbol - Copa Sudamericana' },
+    { key: 'soccer_copa_sudamericana', title: 'Fútbol - Copa Sudamericana' },
     { key: 'soccer_spain_la_liga', title: 'Fútbol - La Liga España' },
-    { key: 'soccer_epl', title: 'Fútbol - Premier League' },
-    { key: 'soccer_uefa_champions_league', title: 'Fútbol - Champions League' },
+    { key: 'soccer_england_premier_league', title: 'Fútbol - Premier League' },
+    { key: 'soccer_uefa_champs_league', title: 'Fútbol - Champions League' },
     { key: 'basketball_nba', title: 'Baloncesto - NBA' },
-    { key: 'tennis_wimbledon', title: 'Tenis - Wimbledon' },
+    { key: 'tennis_atp_wimbledon', title: 'Tenis - Wimbledon' },
     { key: 'americanfootball_nfl', title: 'Fútbol Americano - NFL' },
     { key: 'baseball_mlb', title: 'Béisbol - MLB' },
 ];
