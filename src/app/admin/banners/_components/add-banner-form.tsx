57878994b9ea40938621f5035c0aba8304674235
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-13 23:14:32 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/app/admin/banners/_components/add-banner-form.tsx
+++ src/app/admin/banners/_components/add-banner-form.tsx
@@ -1,7 +1,8 @@
 
 'use client';
 
-import { useActionState, useFormStatus, useEffect, useRef, useState } from 'react';
+import { useActionState, useEffect, useRef, useState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
