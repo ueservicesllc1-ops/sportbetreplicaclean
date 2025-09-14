ac8834aac0042f9db635d2d5898ae9e6657ca4a2
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-14 04:53:20 +0000

    Try fixing this error: `Runtime Error: Error: Failed to read credentials

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,10 @@
-
 import * as admin from 'firebase-admin';
+import { getApplicationDefault } from 'firebase-admin/app';
 
 // Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
     admin.initializeApp({
+        credential: getApplicationDefault(),
         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
 }
