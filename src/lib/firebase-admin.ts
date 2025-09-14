1b13c4bc1562fb9533f29d7ec9607ab1b201fc4f
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-14 04:52:25 +0000

    Error The default Firebase app does not exist. Make sure you call initia

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,16 +3,9 @@ import * as admin from 'firebase-admin';
 
 // Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
-  try {
-    // When initialized without arguments, the SDK will look for the 
-    // `GOOGLE_APPLICATION_CREDENTIALS` environment variable.
-    // In App Hosting, this is configured automatically.
     admin.initializeApp({
         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
-  } catch (error: any) {
-    console.error('Firebase admin initialization error', error.stack);
-  }
 }
 
 export default admin;
