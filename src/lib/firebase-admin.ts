444c186df5d72834be1d9f003fa00543fab9e519
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-14 04:54:13 +0000

    Error (0 , __TURBOPACK__imported__module__$5b$externals$5d2f$firebase$2d

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,11 @@
+
+'use server';
+
 import * as admin from 'firebase-admin';
-import { getApplicationDefault } from 'firebase-admin/app';
 
 // Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: getApplicationDefault(),
         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
 }
