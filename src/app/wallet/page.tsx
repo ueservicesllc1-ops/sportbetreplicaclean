65c61ff75d74e74643d2401e6be9b86995a6c85b
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-14 19:32:02 +0000

    no puedo ver hasta abaj o de l apagina no tiene scroo;

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 export default function WalletPage() {
   
   return (
-    <main className="container mx-auto max-w-2xl py-12">
+    <div className="container mx-auto max-w-2xl py-12">
         <div className='flex items-center justify-between mb-8'>
          <h1 className="text-3xl font-bold tracking-tight">Mi Billetera</h1>
          <Button asChild variant="outline">
@@ -17,6 +17,6 @@ export default function WalletPage() {
         </Button>
       </div>
       <WalletSheet />
-    </main>
+    </div>
   );
 }
